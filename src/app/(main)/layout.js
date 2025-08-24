3f0bbaf4611ef6f12236ad8c2791f2b69010de25
Jhun Mark Romano <jhunmarkromano2@gmail.com>
2025-08-24 13:43:27 +0800

    Homepage UI enhancements
    • Added interview card to the homepage
    • Added icon for interview card
    • Added date and score display in interview card
    • Added banner on the homepage

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,8 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const MainLayout = ({children}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href={"/"} className='flex items-center gap-2'>
+          <Image src={"/logo.svg"} alt='Logo' width={38} height={32}/>
+          <h2 className='text-primary-100'> HireReady AI </h2>
+        </Link>
+        {children}
+      </nav>
+
+    </div>
   )
 }
 export default MainLayout
--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { dummyInterviews, interviewer } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import InterviewCard from "@/components/InterviewCard";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
-export default HomePage
+    <>
+      <section className="card-cta mt-5">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready With AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview question & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href={"/interview"}>Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src={"/robot.png"}
+          alt="Robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+             <section className="flex flex-col gap-6 mt-8">
+         <h2>Your Interview</h2>
+         <div className="interviews-section">
+           {dummyInterviews.map((interview) => (
+             <InterviewCard {...interview} key={interview.id} />
+           ))}
+           {/* <p>You haven't taken any interviews yet</p> */}
+         </div>
+       </section>
+       <section className="flex flex-col gap-6 mt-8">
+         <h2>Take an Interview</h2>
+
+         <div className="interviews-section">
+           {dummyInterviews.map((interview) => (
+             <InterviewCard {...interview} key={interview.id} />
+           ))}
+         </div>
+       </section>
+    </>
+  );
+};
+export default HomePage;
