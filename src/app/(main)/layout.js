74c708103b4724d0dd49f8063f32b07c5279451d
Jhun Mark Romano <jhunmarkromano2@gmail.com>
2025-08-30 21:05:07 +0800

    Fix Navbar Added Loader compoenent
    Added a Menu in the Nav for the user to logout added a new component for the loading page

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,28 +1,13 @@
-import { signOut } from '@/lib/actions/auth.actions'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Navbar from '@/components/Nav';
+import React from 'react';
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
     <div className='root-layout'>
-      <nav>
-        <Link href={"/"} className='flex items-center gap-2'>
-          <Image src={"/logo.svg"} alt='Logo' width={38} height={32}/>
-          <h2 className='text-primary-100'> HireReady AI </h2>
-        </Link>
-        {/* <Link href={"/sign-in"}>
-        <button
-          className="ml-4 px-4 py-2 bg-red-600 text-white rounded"
-          onClick={signOut}
-        >
-          Logout
-        </button>
-        </Link> */}
-        {children}
-      </nav>
-
+      <Navbar/>
+      {children}
     </div>
-  )
-}
-export default MainLayout
+  );
+};
+
+export default MainLayout;
