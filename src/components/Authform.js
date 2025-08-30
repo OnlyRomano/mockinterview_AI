6419d1553268b74a2e34816c095bb70094958d16
Jhun Mark Romano <jhunmarkromano2@gmail.com>
2025-08-30 20:20:28 +0800

    Update the input & Label and Added a 3D Component in the Dashaboard

--- src/components/Authform.js
+++ src/components/Authform.js
@@ -11,6 +11,8 @@ import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/auth.actions";
+import { useState } from "react";
+import { Eye, EyeOff, Loader, Loader2 } from "lucide-react";
 
 const authFormSchema = (type) => {
   return z.object({
@@ -23,6 +25,8 @@ const authFormSchema = (type) => {
 const Authform = ({ type }) => {
   const router = useRouter();
   const formSchema = authFormSchema(type);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
   const form = useForm({
@@ -34,7 +38,12 @@ const Authform = ({ type }) => {
     },
   });
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   async function onSubmit(values) {
+    setIsLoading(true);
     try {
       if (type === "sign-up") {
         const { email, name, password } = values;
@@ -69,6 +78,8 @@ const Authform = ({ type }) => {
     } catch (error) {
       console.log(error);
       toast.error(`Something went wrong: ${error.message}`);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -121,21 +132,44 @@ const Authform = ({ type }) => {
               control={form.control}
               name="email"
               label="Email"
-              placeholder="Example@email.cgom"
+              placeholder="e.g Example@email.com"
               type="email"
             />
 
             {/* Password Input */}
-            <FormField
-              control={form.control}
-              name="password"
-              label="Password"
-              placeholder="•••••••••"
-              type="password"
-            />
+            <div className="relative">
+              <FormField
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="•••••••••"
+                type={showPassword ? "text" : "password"}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                style={{ marginTop: "12px" }}
+              >
+                {showPassword ? (
+                  <EyeOff className="h-5 w-5" />
+                ) : (
+                  <Eye className="h-5 w-5" />
+                )}
+              </button>
+            </div>
 
-            <Button type="submit" className="btn">
-              {isSignIn ? "Sign In" : "Create an Account"}
+            <Button type="submit" className="btn w-full" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <Loader className="size-4 animate-spin" />
+                  {isSignIn ? "Signing In..." : "Creating Account..."}
+                </>
+              ) : (
+                <>
+                  {isSignIn ? "Sign In" : "Create an Account"}
+                </>
+              )}
             </Button>
           </form>
         </Form>
