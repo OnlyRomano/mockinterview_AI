39e4e89478110e4e37eb9725e81e05a4484ad008
Jhun Mark Romano <jhunmarkromano2@gmail.com>
2025-09-07 15:27:13 +0800

    Fix the userId in the interview models

--- src/app/(main)/interview/page.js
+++ src/app/(main)/interview/page.js
@@ -5,11 +5,12 @@ import React from 'react'
 const page = async () => {
 
   const user = await getCurrentUser()
+  
   return (
     <>
         <h3>Interview Generation</h3>
 
-        <Agent userName={user?.name} userId={user?._id} type="generate"/>
+        <Agent userName={user?.name} userId={user?.id} type="generate"/>
     </>
   )
 }
